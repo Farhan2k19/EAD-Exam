b177d16e201702cd6a60b9785b9b14a635a7cbff
Farhan <farhanahmed2k177@gmail.com>
2023-05-27 17:01:34 +0500

    Add App component

--- src/App.js
+++ src/App.js
@@ -4,16 +4,27 @@ import UserParticipation from './Components/UserParticipation';
 function App() {
 
 
+  const poll = {
+    question: "What is your favourite programming language?",
+    options: [
+      { text: "C++", count: 0 },
+      { text: "Java", count: 0 },
+      { text: "Python", count: 0 },
+      { text: "JavaScript", count: 0 },
+    ],
+  };
   
   return (
 
     <>
     <div className="App">
       <h1>
-        Polling App
+        Polling App 
       </h1>
     </div>
     <PollDisplay  />
+   
+
     
 
     </>
